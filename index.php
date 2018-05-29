a63832ed2125ce27f70792605774d59c661a58db
Javaneh-Mohseni <39635919+Javaneh-Mohseni@users.noreply.github.com>
2018-05-28 20:02:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title></title>
+  <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+  <link href='styles/main.css' rel='stylesheet' type="text/css">
+  <!-- <script src="vendor/angular.min.js" type="text/javascript"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.5/angular.min.js"></script>
+  <script src="scripts/app.js" type="text/javascript"></script>
+  <script src="scripts/controllers/main.js" type="text/javascript"></script>
+  <script src="scripts/services/data.js" type="text/javascript"></script>
+  <script src="scripts/directives/todos.js" type="text/javascript"></script>
+  <!-- <script src="scripts/hello-world.js" type="text/javascript"></script> -->
+</head>
+<body ng-app="todoListApp">
+  <todos></todos>
+</body>
+</html>
